bd5bd7f64a886dfa7916995a6dce935564a97f74
thomas_aspa <aspa.thomas@gmail.com>
2023-04-03 16:07:29 +0200

    classe Sejour

---
+++ src/main/java/com/example/reservation_service/Sejour.java
@@ -0,0 +1,15 @@
+package com.example.reservation_service;
+
+import java.util.Date;
+
+public class Sejour {
+
+    private Date dateDebut;
+    private Date dateArrive;
+    private int prix;
+    private String lieu;
+    private String titre;
+    private int nbPersonnes;
+    private String hote;
+
+}
